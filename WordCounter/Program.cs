7dc9cb47e9023583025e7105c6f27a089ad91455
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-05-15 13:21:51 -0700

    add logic for repeating rounds

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -10,7 +10,7 @@ namespace Program
       for (int i = 0; i < str.Length; i++)
       {
         Console.Write(str[i]);
-        System.Threading.Thread.Sleep(50);
+        System.Threading.Thread.Sleep(40);
       }
       Console.WriteLine();
     }
@@ -26,6 +26,26 @@ namespace Program
     static void Main()
     {
       TypeLine("Welcome to the word counter!");
+      StartWordCounter();
+      TypeLine("Would you like to count another word? Y/N");
+      tryagain:
+      string continueQuestion = Console.ReadLine().ToLower();
+      while (continueQuestion == "y" || continueQuestion == "yes")
+      {
+        StartWordCounter();
+        TypeLine("Would you like to count another word? Y/N");
+        continueQuestion = Console.ReadLine().ToLower();
+      }
+      while (continueQuestion != "n" && continueQuestion != "no")
+      {
+          TypeLine("Sorry, I didn't catch that! Would you like to count another word? Y/N");
+          goto tryagain;
+      }
+      TypeLine("Have a nice day!");
+    }
+
+    static void StartWordCounter()
+    {
       TypeLine("Please enter a word to count: ");
       string userWord = Console.ReadLine();
 
@@ -48,7 +68,6 @@ namespace Program
       int count = userEntry.CountRepeats();
       TypeLineSlow("Calculating...");
       TypeLine("There are " + count + " instances of the word " + userWord + " in your sentence.");
-      TypeLine("Have a nice day!");
     }
   }
 }
