fd1d3bcc7c40d3d0e634f4c13c23377561835a12
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-05-15 12:37:48 -0700

    add empty input loops

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -10,8 +10,22 @@ namespace Program
       Console.WriteLine("Welcome to the word counter!");
       Console.Write("Please enter a word to count: ");
       string userWord = Console.ReadLine();
+
+      while (userWord == "")
+      {
+        Console.Write("Don't be shy! Please enter your word: ");
+        userWord = Console.ReadLine();
+      }
+
       Console.Write("Please enter your sentence to look through: ");
       string userSentence = Console.ReadLine();
+
+      while (userSentence == "")
+      {
+        Console.Write("I'm sure you can think of a sentence to look through! Try again: ");
+        userSentence = Console.ReadLine();
+      }
+
       Entry userEntry = new Entry(userWord, userSentence);
       int count = userEntry.CountRepeats();
       Console.WriteLine("Calculating...");
