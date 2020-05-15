54acf3ab520f3a73a2bb82aeec9318aaeb6dea2f
Jessica Hvozdovich <jhvozdovich@gmail.com>
2020-05-15 12:49:36 -0700

    add typing display logic

--- WordCounter/Program.cs
+++ WordCounter/Program.cs
@@ -5,32 +5,50 @@ namespace Program
 {
   public class WordCounter
   {
+    public static void TypeLine(string str)
+    {
+      for (int i = 0; i < str.Length; i++)
+      {
+        Console.Write(str[i]);
+        System.Threading.Thread.Sleep(50);
+      }
+      Console.WriteLine();
+    }
+    public static void TypeLineSlow(string str)
+    {
+      for (int i = 0; i < str.Length; i++)
+      {
+        Console.Write(str[i]);
+        System.Threading.Thread.Sleep(100);
+      }
+      Console.WriteLine();
+    }
     static void Main()
     {
-      Console.WriteLine("Welcome to the word counter!");
-      Console.Write("Please enter a word to count: ");
+      TypeLine("Welcome to the word counter!");
+      TypeLine("Please enter a word to count: ");
       string userWord = Console.ReadLine();
 
       while (userWord == "")
       {
-        Console.Write("Don't be shy! Please enter your word: ");
+        TypeLine("Don't be shy! Please enter your word: ");
         userWord = Console.ReadLine();
       }
 
-      Console.Write("Please enter your sentence to look through: ");
+      TypeLine("Please enter your sentence to look through: ");
       string userSentence = Console.ReadLine();
 
       while (userSentence == "")
       {
-        Console.Write("I'm sure you can think of a sentence to look through! Try again: ");
+        TypeLine("I'm sure you can think of a sentence to look through! Try again: ");
         userSentence = Console.ReadLine();
       }
 
       Entry userEntry = new Entry(userWord, userSentence);
       int count = userEntry.CountRepeats();
-      Console.WriteLine("Calculating...");
-      Console.WriteLine("There are " + count + " instances of the word " + userWord + " in your sentence.");
-      Console.WriteLine("Have a nice day!");
+      TypeLineSlow("Calculating...");
+      TypeLine("There are " + count + " instances of the word " + userWord + " in your sentence.");
+      TypeLine("Have a nice day!");
     }
   }
 }
